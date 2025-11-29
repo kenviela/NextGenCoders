ece8373a315db8d36d4d68132917d931d674b9e1
Daniela <danielaguzman50@outlook.es>
2025-11-29 13:04:13 -0500

    se ajusto rutas para despliegue

--- public/js/footer.js
+++ public/js/footer.js
@@ -1,8 +1,9 @@
 // llamar al footer
-fetch("./components/Footer/Footer.html")
-  .then((res) => res.text())
-  .then((html) => {
-    const container = document.createElement("div");
-    container.innerHTML = html;
-    document.body.appendChild(container);
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("./components/Footer/Footer.html")
+    .then((res) => res.text())
+    .then((data) => {
+      document.getElementById("footer").innerHTML = data;
+    })
+    .catch((error) => console.error("Error cargando footer:", error));
+});
--- public/js/header.js
+++ public/js/header.js
@@ -1,6 +1,9 @@
 // Cargar el header a la vista
-fetch("./components/Header/Header.html")
-  .then((res) => res.text())
-  .then((data) => {
-    document.getElementById("header").innerHTML = data;
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("./components/Header/Header.html")
+    .then((res) => res.text())
+    .then((data) => {
+      document.getElementById("header").innerHTML = data;
+    })
+    .catch((error) => console.error("Error cargando header:", error));
+});
