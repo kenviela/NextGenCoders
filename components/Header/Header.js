2401b10ad44a734a59087e7a90ee7b94d1188411
Daniela <danielaguzman50@outlook.es>
2025-11-30 20:09:35 -0500

    conflicto resuelto

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,20 +1,20 @@
 // Revisa si estamos en inicio
-const isHome =
+/* const isHome =
   window.location.pathname.endsWith("index.html") ||
-  window.location.pathname === "/";
+  window.location.pathname === "/"; */
 
 const menuBtn = document.getElementById("menuBtn");
 const navMenu = document.getElementById("navMenu");
-const homeLink = document.getElementById("homeLink");
+/* const homeLink = document.getElementById("homeLink"); */
 
-// Ocultar HOME si estamos en index
+/* // Ocultar HOME si estamos en index
 if (isHome) {
   homeLink.style.display = "none";
 } else {
   homeLink.style.display = "";
-}
+} */
 
 // Toggle del menú hamburguesa
 menuBtn.addEventListener("click", () => {
-  navMenu.classList.toggle("open");
+  navMenu.classList.toggle("hidden");
 });
